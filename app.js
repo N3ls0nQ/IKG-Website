712bf9c3b2da6399b96c096c0b57863da2cd0592
N3ls0nQ <quentin.hoehne062@gmail.com>
2022-05-12 22:29:57 +0200

    Added Content for "Schule"

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 window.addEventListener("scroll", () => {
     document.querySelector(".navbar").style.top = "0px"
 });
+
+var section = document.querySelector(".placeholder__section");
+window.addEventListener("scroll", () => {
+    var value = window.scrollY;
+    console.log(value)
+    section.style.clipPath = "circle("+ value +"px at center)"
+})
